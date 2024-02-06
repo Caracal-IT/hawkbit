f7783785489f05673861310e4b0028e417936124
Mare Ettiene <Mare.Ettiene@bakerhughes.com>
2024-02-06 14:49:12 +0200

    Add MQTT

--- src/libs/update-managers/Caracal.Device.UpdateManager/Clients/MqttClient.cs
+++ src/libs/update-managers/Caracal.Device.UpdateManager/Clients/MqttClient.cs
@@ -1,6 +1,30 @@
-﻿namespace Caracal.Device.UpdateManager.Clients;
+﻿using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Extensions.ManagedClient;
+
+namespace Caracal.Device.UpdateManager.Clients;
 
 public class MqttClient
 {
-    
+    private IManagedMqttClient _mqttClient = new MqttFactory().CreateManagedMqttClient();
+
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        var options = new MqttClientOptionsBuilder()
+            .WithTcpServer("localhost", 1883)
+            .WithClientId(Guid.NewGuid().ToString());
+
+        var managedOptions = new ManagedMqttClientOptionsBuilder()
+            .WithAutoReconnectDelay(TimeSpan.FromSeconds(5))
+            .WithClientOptions(options);
+        
+        _mqttClient.ApplicationMessageReceivedAsync += MqttClientOnApplicationMessageReceivedAsync;
+        
+        return Task.CompletedTask;
+    }
+
+    private Task MqttClientOnApplicationMessageReceivedAsync(MqttApplicationMessageReceivedEventArgs arg)
+    {
+        return Task.CompletedTask;
+    }
 }
